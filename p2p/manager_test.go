64abdb36b271ba1348403a4b7987f244745c6982
Quankey <aldiansosmed@gmail.com>
2026-08-13 07:40:35 +0000

    first commit4

--- p2p/manager_test.go
+++ p2p/manager_test.go
@@ -1,8 +1,17 @@
-// Copyright (c) 2024-2026 The Xcosh Contributors
-// Xcosh is an experiment in modularity, designed to improve on the work
-// of Satoshi Nakamoto and to inspire more creative genius in the space.
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2026 AldianOkto. All rights reserved.
+// Copyright (c) 2026 Eterbit Core.
+// Use of this source code is governed by the Apache License.
+// that can be found in the root directory of this repository.
+// Project: Eterbit / Blockchain Core
+//
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package p2p
 
